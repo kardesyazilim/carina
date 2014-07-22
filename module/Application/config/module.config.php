6c929ee8784826f4b63392b6bd99ce89be9e5cda
Kardeş Yazılım <kardesyazilim@gmail.com>
2014-07-22 17:23:32 +0300

    add config file
    add config file

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -78,7 +78,7 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/application/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'application/index/add' => __DIR__ . '/../view/application/index/add.phtml',
+            'add' => __DIR__ . '/../view/application/index/add.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
